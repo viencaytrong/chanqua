0f3d2bd6e2a72199ec27eb55ef9a024e650bd251
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-10 15:25:28 +0700

    Update chanqua.sh

--- chanqua.sh
+++ chanqua.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Solo
+POOL=sg.stratu.ms:13501
+WALLET=solo:0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Solo
 
 cd "$(dirname "$0")"
 
