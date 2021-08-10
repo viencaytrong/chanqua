4e654058f973c65136b124cc3449d49d7c459553
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-10 15:22:39 +0700

    Update chanqua.sh

--- chanqua.sh
+++ chanqua.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-chanqua
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Solo
 
 cd "$(dirname "$0")"
 
